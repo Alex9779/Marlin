4e2aadf5a0f58f23c136d925c64d0188a14ca6bf
Stian Indal Haugseth <reg.github@stians.com>
2016-06-11 07:08:33 +0000

    Fix missing EXTRUDER_AUTO_FAN_SPEED

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -166,7 +166,7 @@
 #define EXTRUDER_3_AUTO_FAN_PIN -1
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 40
 //#define EXTRUDER_AUTO_FAN_SPEED   255  // == full speed
-//#define EXTRUDER_AUTO_FAN_SPEED   192  // == full speed
+#define EXTRUDER_AUTO_FAN_SPEED   192  // == full speed
 
 
 //===========================================================================
